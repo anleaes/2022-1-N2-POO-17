7bbd4332275ef838f70c284518d467a89ab1d7ff
Suelen CSantos <suelencsantos.scs@gmail.com>
2022-06-13 15:31:33 -0300

    class

--- paciente.py
+++ paciente.py
@@ -1,13 +1,15 @@
+from pessoa import Pessoa
+from dados import Dados
+
 class Paciente(Pessoa, Dados):
     
-    def _init_(self, plano_saude, acompanhante, tempo_internacao, exames, contato_emergencia, nome, idade, endereco, telefone, email, tipo_sanguineo, alergia, uso_ilicito, uso_medicamento):
+    def _init_(self, plano_saude, acompanhante, tempo_internacao, dieta, exames, contato_emergencia, nome, idade, endereco, telefone, email, tipo_sanguineo, alergia, uso_ilicito, uso_medicamento):
         super()._init__init_(nome, idade, endereco, telefone, email)
         super()._init__init_(tipo_sanguineo, alergia, uso_ilicito, uso_medicamento)
         self._plano_saude = plano_saude
         self._acompanhante = acompanhante
         self._tempo_internacao = tempo_internacao
-        self._telefone = telefone
-        self._email = email
+        self._dieta = dieta
         self._exames = exames
         self._contato_emergencia = contato_emergencia
         
